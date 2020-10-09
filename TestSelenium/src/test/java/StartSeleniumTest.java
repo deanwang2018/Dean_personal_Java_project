4be498ebaf45a4788d5bc1a7a4246e3b0eeb60d4
deanwang2018 <wd8352@163.com>
2020-10-09 10:10:33 +0800

    add selenium sample

---
+++ TestSelenium/src/test/java/StartSeleniumTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * @author wangdian
+ * @package PACKAGE_NAME
+ * @date 2020/10/9
+ * @time 9:58
+ */
+public class StartSeleniumTest {
+
+    @Test
+    public void startSelenium() {
+        WebDriver wd = new ChromeDriver();
+
+//        System.setProperty("Webdriver.chrome.driver",
+//                "D:\\Dean_working_dir\\working_software_target\\chromedriver_win32");
+
+        wd.get("https://home.testing-studio.com");
+
+        wd.findElement(By.xpath("//span[contains(text(),'登录')]")).click();
+    }
+}
